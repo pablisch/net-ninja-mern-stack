4425c887bf0a0f69ea1e780201c36531ef0e539a
Pablo <pablito@me.com>
2023-07-31 21:49:36 +0100

    created dev command to start nodemon

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+// Create the Express app.
+const app = express();
+
+// Define the port.
+app.listen(4000, () => {
+  console.log('Server listening on port 4000.');
+});
