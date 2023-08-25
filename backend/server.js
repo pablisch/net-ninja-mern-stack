9a4c7e7e36f3a55c210c1a2382644c0772f1909a
Pablo <pablito@me.com>
2023-08-25 23:07:21 +0100

    Added mongoose to project

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutsRoutes = require('./routes/workouts');
+
+const mongoUri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.suchhmq.mongodb.net/?retryWrites=true&w=majority`
 
 // Create the Express app.
 const app = express();
 
 // Define the middleware.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
   console.log('path:', req.path);
   console.log('method:', req.method);
@@ -16,7 +22,18 @@ app.use((req, res, next) => {
 // Route.
 app.use('/api/workouts', workoutsRoutes);
 
-// Define the port.
-app.listen(process.env.PORT, () => {
-  console.log('Server listening on port', process.env.PORT);
-}); 
+// Connect to the database
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Connected to the database');
+    // Define the port.
+    app.listen(process.env.PORT, () => {
+      console.log('Server listening on port', process.env.PORT);
+    }); 
+  })
+  .catch(err => console.log(err));
+
+  
